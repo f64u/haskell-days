70311d0dd843ec7a9c5b3f70e8c385c12739fc67
Fady Adal <2masadel@gmail.com>
2021-10-20 13:22:47 -0500

    turns out I can; somewhat obvious functionlization; looking at parse

--- lab03/Lab3.hs
+++ lab03/Lab3.hs
@@ -12,41 +12,31 @@ eval (Plus l r) = eval l + eval r
 eval (Mult l r) = eval l * eval r
 eval (Div  l r) = eval l / eval r
 
-data Token = TInt Integer | TPlus | TNeg | TMult | TDiv | TParen [Token] deriving (Show, Eq)
+data Token = TInt Integer | TNeg | TPlus | TMult | TDiv | TParen [Token] deriving (Show, Eq)
 
 -- Lookup table for our tokinzer
 lookup' :: Char -> Token
 lookup' c | isDigit c = TInt (fromIntegral $ digitToInt c)
-          | c == '+'  = TPlus
           | c == '-'  = TNeg
+          | c == '+'  = TPlus
           | c == '*'  = TMult
           | c == '/'  = TDiv
-          | otherwise = error ("This shouldn't happen: got " ++ show c)
+          | otherwise = error $ "This shouldn't happen: got " ++ show c
 
 tokenize :: String -> [Token]
-tokenize = fst . tokenizeTillClose
+tokenize = fst . tokenizeTillClose . filter (not . isSpace)
 
-tokenizeTillClose :: String -> ([Token], String) -- I feel like I haven't gotten out my procedural style of thinking,
-                                                 -- and this shows a bit (a lot?) here, but I do not think I'll be able to 
-                                                 -- functionlize this more than that. parse too.
-tokenizeTillClose s =
-  let (prelparen, lparenon) = break (== '(') s -- break on the first occurance of an opening parenthesis
-  in  if null lparenon
-           || (case (')' `elemIndex` s, '(' `elemIndex` s) of -- If no more opening parentheses inside
-                (Just r, Just l) -> l > r
-                _                -> False
-              )
-        then
-          let (prerparen, rparenon) = break (== ')') s
-          in  (tokenizeParenless prerparen, tail rparenon) -- then close the parenthesis and return the rest
-        else prependToks -- However, if more opening parentheses are inside
-          (tokenizeParenless prelparen) -- then add whatever was before it
-          (let (tokens, rest) = tokenizeTillClose $ tail lparenon -- tokenize whatever inside
-           in  prependToks [TParen tokens] (tokenizeTillClose rest) -- and continue tokenizing the rest
-          )
- where
-  tokenizeParenless = map lookup' . filter (not . isSpace)
-  prependToks toks1 (toks2, rest) = (toks1 ++ toks2, rest)
+tokenizeTillClose :: String -> ([Token], String)
+tokenizeTillClose ""           = ([], "")
+tokenizeTillClose (')' : rest) = ([], rest)
+tokenizeTillClose ('(' : rest) =
+  let (tokens, otherRest) = tokenizeTillClose rest
+  in  prependTok (TParen tokens) (tokenizeTillClose otherRest)
+tokenizeTillClose (char : rest) =
+  prependTok (lookup' char) $ tokenizeTillClose rest
+
+prependTok :: a -> ([a], b) -> ([a], b)
+prependTok tok (toks, rest) = (tok : toks, rest)
 
 parse :: [Token] -> ArithExp
 parse tokens =
