43b30c8711ddc73d6484199c4e26400b0bbc9c00
Fady Adal <2masadel@gmail.com>
2021-10-20 12:55:53 -0500

    added comments; a bit of pruning

--- lab03/Lab3.hs
+++ lab03/Lab3.hs
@@ -2,7 +2,6 @@ module Lab3 where
 
 import           Data.Char
 import           Data.List
-import           Data.Maybe
 import           Data.Ratio
 
 data ArithExp = Number Rational | Plus ArithExp ArithExp | Mult ArithExp ArithExp | Div ArithExp ArithExp deriving (Show, Eq)
@@ -22,27 +21,28 @@ lookup' c | isDigit c = TInt (fromIntegral $ digitToInt c)
           | c == '-'  = TNeg
           | c == '*'  = TMult
           | c == '/'  = TDiv
-          | otherwise = error "This shouldn't happen"
+          | otherwise = error ("This shouldn't happen: got " ++ show c)
 
 tokenize :: String -> [Token]
 tokenize = fst . tokenizeTillClose
 
-tokenizeTillClose :: String -> ([Token], String)
-tokenizeTillClose "" = ([], "")
+tokenizeTillClose :: String -> ([Token], String) -- I feel like I haven't gotten out my procedural style of thinking,
+                                                 -- and this shows a bit (a lot?) here, but I do not think I'll be able to 
+                                                 -- functionlize this more than that. parse too.
 tokenizeTillClose s =
-  let (prelparen, lparenon) = break (== '(') s
+  let (prelparen, lparenon) = break (== '(') s -- break on the first occurance of an opening parenthesis
   in  if null lparenon
-           || (case (')' `elemIndex` s, '(' `elemIndex` s) of
+           || (case (')' `elemIndex` s, '(' `elemIndex` s) of -- If no more opening parentheses inside
                 (Just r, Just l) -> l > r
                 _                -> False
               )
         then
           let (prerparen, rparenon) = break (== ')') s
-          in  (tokenizeParenless prerparen, tail rparenon)
-        else prependToks
-          (tokenizeParenless prelparen)
-          (let (tokens, rest) = tokenizeTillClose (tail lparenon)
-           in  prependToks [TParen tokens] (tokenizeTillClose rest)
+          in  (tokenizeParenless prerparen, tail rparenon) -- then close the parenthesis and return the rest
+        else prependToks -- However, if more opening parentheses are inside
+          (tokenizeParenless prelparen) -- then add whatever was before it
+          (let (tokens, rest) = tokenizeTillClose $ tail lparenon -- tokenize whatever inside
+           in  prependToks [TParen tokens] (tokenizeTillClose rest) -- and continue tokenizing the rest
           )
  where
   tokenizeParenless = map lookup' . filter (not . isSpace)
@@ -63,9 +63,8 @@ parse tokens =
                   else Div (parse $ init l_multdiv) (parse r_multdiv)
         else Plus (parse l_plus) (parse $ tail r_plus)
  where
-  parseOpless (TParen tokens : _) = parse tokens
+  parseOpless (TParen tokens : _) = parse tokens -- TParen can only be surrounded with ops, so if we got here, nothing is beside it
   parseOpless (TNeg : ns) = let (Number n) = parseOpless ns in Number (-n)
   parseOpless ns = Number . sum $ zipWith (\i (TInt n) -> 10 ^ i * (n % 1))
                                           (reverse [0 .. length ns - 1])
                                           ns
-
