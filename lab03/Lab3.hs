3de3ff2d3d8994b1b820676033841f2dd3c5d5b3
Fady Adal <2masadel@gmail.com>
2021-10-20 19:14:23 -0500

    dude i can't do no more

--- lab03/Lab3.hs
+++ lab03/Lab3.hs
@@ -1,52 +1,38 @@
 module Lab3 where
-import           Data.Char
-import           Data.Maybe
-import           Data.Ratio
+
+import Data.Char
+import Data.Maybe
+import Data.Ratio
 
 data ArithExp = Number Rational | Op Char ArithExp ArithExp deriving (Show, Eq)
 
 data Token = TInt Integer | TOp Char | TParen [Token] deriving (Show, Eq)
-charTable = [('+', (+)), ('*', (*)), ('/', (/))]
 
 eval :: ArithExp -> Rational
-eval (Number n ) = n
-eval (Op op l r) = fromJust (lookup op charTable) (eval l) (eval r)
+eval (Number n) = n
+eval (Op op l r) = fromJust (lookup op [('+', (+)), ('*', (*)), ('/', (/))]) (eval l) (eval r)
 
 tokenize :: String -> [Token]
 tokenize = fst . tokenizeTillClose . filter (not . isSpace)
-
-tokenizeTillClose :: String -> ([Token], String)
-tokenizeTillClose ""           = ([], "")
-tokenizeTillClose (')' : rest) = ([], rest)
-tokenizeTillClose ('(' : rest) =
-  let (tokens, otherRest) = tokenizeTillClose rest
-  in  prependTok (TParen tokens) (tokenizeTillClose otherRest)
-tokenizeTillClose (char : rest) = prependTok (tokLookup char)
-  $ tokenizeTillClose rest
- where
-  tokLookup char | isDigit char = TInt (fromIntegral $ digitToInt char)
-                 | otherwise    = TOp char
-
-prependTok :: a -> ([a], b) -> ([a], b)
-prependTok tok (toks, rest) = (tok : toks, rest)
+  where
+    tokenizeTillClose "" = ([], "")
+    tokenizeTillClose (')' : rest) = ([], rest)
+    tokenizeTillClose ('(' : rest) =
+      let (toks, rest') = tokenizeTillClose rest
+       in prependTok (TParen toks) (tokenizeTillClose rest')
+    tokenizeTillClose (char : rest) =
+      prependTok (if isDigit char then TInt (fromIntegral $ digitToInt char) else TOp char) $
+        tokenizeTillClose rest
+    prependTok tok (toks, rest) = (tok : toks, rest)
 
 parse :: [Token] -> ArithExp
-parse tokens =
-  let (l_plus, r_plus) = break (== TOp '+') tokens -- thank god + is commutative
-  in  if null r_plus
-        then
-          let (r_multdiv', l_multdiv') =
-                break (`elem` [TOp '*', TOp '/']) (reverse tokens)
-              (r_multdiv, l_multdiv) = (reverse r_multdiv', reverse l_multdiv') -- double reverses I know; performance isn't a concern I presume?
-          in  if null l_multdiv
-                then parseOpless tokens
-                else Op (let (TOp char) = last l_multdiv in char)
-                        (parse $ init l_multdiv)
-                        (parse r_multdiv)
-        else Op '+' (parse l_plus) (parse $ tail r_plus)
- where
-  parseOpless (TParen tokens : _) = parse tokens -- TParen can only be surrounded with ops, so if we got here, nothing is beside it
-  parseOpless (TOp '-' : ns) = let (Number n) = parseOpless ns in Number (-n)
-  parseOpless ns = Number . sum $ zipWith (\i (TInt n) -> 10 ^ i * (n % 1))
-                                          (reverse [0 .. length ns - 1])
-                                          ns
+parse toks = case (null rplus, null lmultdiv) of
+  (True, True) -> parseOpless toks
+  (True, _) -> Op (let (TOp char) = last lmultdiv in char) (parse $ init lmultdiv) (parse rmultdiv)
+  _ -> Op '+' (parse lplus) (parse $ tail rplus)
+  where
+    ((lplus, rplus), (rmultdiv, lmultdiv)) = (break (== TOp '+') toks, (\f (a, b) -> (f a, f b)) reverse $ break (`elem` [TOp '*', TOp '/']) (reverse toks))
+    parseOpless (TParen toks:_) = parse toks
+    parseOpless (TOp '-':toks) = let (Number n) = parseOpless toks in Number (- n)
+    parseOpless toks =
+      Number . sum $ zipWith (\i (TInt n) -> 10 ^ i * (n % 1)) (reverse [0 .. length toks - 1]) toks
