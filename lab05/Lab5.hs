b71d34130c8cef3cba00283d21fd2bf530cb31f0
Fady Adal <2masadel@gmail.com>
2021-11-04 20:10:31 -0500

    basic functioning lab

--- lab05/Lab5.hs
+++ lab05/Lab5.hs
@@ -1,7 +1,24 @@
-import RandState
-import System.Environment
-import System.IO
-import System.Random
+import           Control.Applicative            ( Applicative(liftA2) )
+import           RandState                      ( RandState
+                                                  ( RandState
+                                                  , runRandState
+                                                  )
+                                                , runRandom
+                                                )
+import           System.Environment             ( getArgs )
+import           System.Random                  ( Random(randomR)
+                                                , StdGen
+                                                , newStdGen
+                                                )
+import           Test.QuickCheck                ( shuffle )
+
+-- Rolling to dices 
+
+-- | Returns the sum of rolling two dices as a monadic RandState Instance
+rollTwoDice :: RandState Int
+rollTwoDice = liftA2 (+) (randR (1, 6)) (randR (1, 6))
+
+-- Shuffling cards
 
 -- Data types to represent playing cards
 data CardValue
@@ -18,72 +35,86 @@ data CardSuit
     | Clubs
     deriving (Show, Eq)
 
-data PlayingCard =
-    PlayingCard CardValue CardSuit
-    deriving (Eq)
+data PlayingCard = PlayingCard CardValue CardSuit
+  deriving Eq
 
 type Deck = [PlayingCard]
 
 
 instance Show PlayingCard where
-    show (PlayingCard value suit) =
-        valueStr value ++ suitStr suit
-        where
-            suitStr Hearts   = "\x1B[31m♥\x1B[0m" -- red in terminal
-            suitStr Diamonds = "\x1B[31m♦\x1B[0m" -- red in terminal
-            suitStr Spades   = "♠"
-            suitStr Clubs    = "♣"
-            -- suitStr Hearts   = "H"  -- uncomment if you don't have Unicode
-            -- suitStr Diamonds = "D"
-            -- suitStr Spades   = "S"
-            -- suitStr Clubs    = "C"
-            valueStr King           = "K"
-            valueStr Queen          = "Q"
-            valueStr Jack           = "J"
-            valueStr (NumberCard n) = show n
+  show (PlayingCard value suit) = valueStr value ++ suitStr suit
+   where
+    suitStr Hearts   = "\x1B[31m♥\x1B[0m" -- red in terminal
+    suitStr Diamonds = "\x1B[31m♦\x1B[0m" -- red in terminal
+    suitStr Spades   = "♠"
+    suitStr Clubs    = "♣"
+    valueStr King           = "K"
+    valueStr Queen          = "Q"
+    valueStr Jack           = "J"
+    valueStr (NumberCard n) = show n
 
 
 -- fullCardDeck is a deck of cards, 52 in total, with a King, a Queen,
 -- a Jack and NumberCards from 1 to 10 for each suit.
 fullCardDeck :: Deck
-fullCardDeck =
-    [ PlayingCard v s | v <- allVals, s <- allSuits ]
-    where
-        allVals  = King : Queen : Jack : [ NumberCard i | i <- [1..10] ]
-        allSuits = [Hearts, Diamonds, Spades, Clubs]
+fullCardDeck = [ PlayingCard v s | v <- allVals, s <- allSuits ]
+ where
+  allVals  = King : Queen : Jack : [ NumberCard i | i <- [1 .. 10] ]
+  allSuits = [Hearts, Diamonds, Spades, Clubs]
+
+-- | Removes a random card from a deck and returns the pair of the card removed and the deck without the card
+removeCard :: Deck -> RandState (PlayingCard, Deck)
+removeCard lst =
+  (\i -> (lst !! i, filter (/= lst !! i) lst)) <$> randR (0, length lst - 1)
+
+shuffleDeck :: Deck -> RandState Deck
+shuffleDeck []  = pure []
+shuffleDeck lst = do
+  (card, deck) <- removeCard lst
+  shuffledDeck <- shuffleDeck deck
+  pure (card : shuffledDeck)
+
+shuffleADeck :: RandState Deck
+shuffleADeck = shuffleDeck fullCardDeck
 
 -- basic random functions
 
 randR :: Random a => (a, a) -> RandState a
-randR = undefined
+randR = RandState . randomR
 
--- -- UNCOMMENT THE FOLLOWING BEFORE SUBMISSION --
---
---
--- shuffleNTimes :: Int -> StdGen -> IO ()
--- shuffleNTimes nTimes gen =
---     undefined
---
--- rollTwoDiceNTimes :: Int -> StdGen -> IO ()
--- rollTwoDiceNTimes nTimes gen =
---     undefined
---
---
--- -- BESIDES UNCOMMENTING, DO NOT MODIFY BELOW THIS LINE --
---
--- usage :: String
--- usage =
---     "Lab 5: Randomizer\n" ++
---     "\n" ++
---     "$ ./Lab5 shuffle 600      # 600 times: output a full deck shuffle\n" ++
---     "$ ./Lab5 rollTwoDice 800  # 800 times: output the sum of rolling two dice\n" ++
---     "\n"
---
--- main :: IO ()
--- main = do
---     gen  <- newStdGen
---     args <- getArgs
---     case args of
---         ["shuffle",     nTimes] -> shuffleNTimes     (read nTimes) gen
---         ["rollTwoDice", nTimes] -> rollTwoDiceNTimes (read nTimes) gen
---         _                       -> putStrLn usage
+-- UNCOMMENT THE FOLLOWING BEFORE SUBMISSION --
+
+-- | Evaluates and outputs to the std output the result of a RandState nTimes times
+doNTimes :: (Show a) => RandState a -> Int -> StdGen -> IO ()
+doNTimes _    0      _   = pure ()
+doNTimes what nTimes gen = do
+  let (result, gen') = runRandState what gen
+  print result
+  doNTimes what (nTimes - 1) gen'
+
+-- | Shuffles the full deck nTimes times and prints the result of each time to the std output
+shuffleNTimes :: Int -> StdGen -> IO ()
+shuffleNTimes = doNTimes shuffleADeck
+
+-- | Prints the sum of two dice rolls nTimes times to the std output
+rollTwoDiceNTimes :: Int -> StdGen -> IO ()
+rollTwoDiceNTimes = doNTimes rollTwoDice
+
+-- BESIDES UNCOMMENTING, DO NOT MODIFY BELOW THIS LINE --
+
+usage :: String
+usage =
+  "Lab 5: Randomizer\n"
+    ++ "\n"
+    ++ "$ ./Lab5 shuffle 600      # 600 times: output a full deck shuffle\n"
+    ++ "$ ./Lab5 rollTwoDice 800  # 800 times: output the sum of rolling two dice\n"
+    ++ "\n"
+
+main :: IO ()
+main = do
+  gen  <- newStdGen
+  args <- getArgs
+  case args of
+    ["shuffle"    , nTimes] -> shuffleNTimes (read nTimes) gen
+    ["rollTwoDice", nTimes] -> rollTwoDiceNTimes (read nTimes) gen
+    _                       -> putStrLn usage
--- lab05/RandState.hs
+++ lab05/RandState.hs
@@ -1,20 +1,28 @@
 module RandState where
 
-import System.Random
+import           System.Random
 
 newtype RandState a = RandState {
   runRandState :: StdGen -> (a, StdGen)
 }
 
 instance Functor RandState where
-  fmap = undefined
-
+  fmap f rs =
+    RandState $ \gen -> let (a, gen') = runRandState rs gen in (f a, gen')
 instance Applicative RandState where
-  pure = undefined
-  (<*>) = undefined
+  pure a = RandState $ (,) a
+  mf <*> ma = RandState $ \gen ->
+    let (f, gen' ) = runRandState mf gen
+        (a, gen'') = runRandState ma gen'
+    in  (f a, gen'')
 
 instance Monad RandState where
-  (>>=) = undefined
+  mra >>= f = RandState $ \gen ->
+    let (a, gen')  = runRandState mra gen
+        mf         = f a
+        (b, gen'') = runRandState mf gen'
+    in  (b, gen'')
+
 
 {- primitive manipulation functions -}
 
@@ -22,7 +30,7 @@ get :: RandState StdGen
 get = RandState $ \gen -> (gen, gen)
 
 put :: StdGen -> RandState ()
-put gen' = RandState $ \gen -> ((), gen')
+put gen = RandState $ const ((), gen)
 
 -- runRandom runs a RandState monad, given an initial random number generator
 -- runRandom is equivalent to evalState
@@ -32,8 +40,4 @@ runRandom (RandState f) s = fst $ f s
 -- rand is a helper function that generates a random instance of any
 --  type in the Random class, using the RandState monad.
 rand :: Random a => RandState a
-rand = do
-    gen <- get
-    let (x, gen') = random gen
-    put gen'
-    return x
+rand = RandState random
